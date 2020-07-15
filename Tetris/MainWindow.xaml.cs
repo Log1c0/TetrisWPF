45bb001394cf5166a55b81cc3c93aef78432f858
Artem <artem@artem.com>
2020-07-15 23:09:21 +0700

    Some refactoring

--- Tetris/MainWindow.xaml.cs
+++ Tetris/MainWindow.xaml.cs
@@ -1,47 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Media;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace Tetris
 {
-    public class Square
-    {
-        public SolidColorBrush Color { get; set; }
-        public bool Filled { get; set; }
-        public Square(SolidColorBrush color, bool filled)
-        {
-            this.Color = color;
-            this.Filled = filled;
-        }
-    }
 
-    public class Figure
-    {
-        public Point [] Cordinates { get; set; }
-        public  SolidColorBrush Color { get; set; }
-        public Figure(Point[] Cordinates, SolidColorBrush color)
-        {
-            this.Cordinates = Cordinates;
-            this.Color = color;
-        }
-
-        public Figure Clone()
-        {
-            Figure clone = (Figure)this.MemberwiseClone();
-            clone.Cordinates = new Point[this.Cordinates.Length];
-            for (int i = 0; i < this.Cordinates.Length; i++)
-            {
-                clone.Cordinates[i] = new Point(this.Cordinates[i].X, this.Cordinates[i].Y);
-            }
-            return clone;
-        }
-    }
+    
 
     public partial class MainWindow : Window
     {
@@ -50,19 +20,20 @@ namespace Tetris
             InitializeComponent();
         }
 
-        private double SquareSize;
-        private int LineSize;
-        private int NextFigureIndex = 0;
+        private double squareSize;
+        private int lineSize;
+        
+        private int nextFigureIndex = 0;
 
         private bool makeNewFigure = true;
 
         private Figure CurrentFigure;
 
-        private DispatcherTimer gameTimer = new DispatcherTimer();
+        private readonly DispatcherTimer gameTimer = new DispatcherTimer();
 
-        private Square[,] _gameField = new Square[10, 20]; //x, y
+        private readonly Square[,] gameField = new Square[10, 20]; //x, y
 
-        private static Figure[] _figureArray = new Figure[7]
+        private static readonly Figure[] FigureArray = new Figure[7]
         {
             new Figure(new Point[] { new Point(3, 0), new Point(4, 0), new Point(5, 0), new Point(6, 0)},
                 (SolidColorBrush) (new BrushConverter().ConvertFrom("#00F0F0"))),
@@ -89,42 +60,42 @@ namespace Tetris
         private static List<Rectangle> _allRect = new List<Rectangle>();
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            for (int x = 0; x < _gameField.GetLength(0); x++)
-                for (int y = 0; y < _gameField.GetLength(1); y++)
-                    _gameField[x, y] = new Square((SolidColorBrush) (new BrushConverter().ConvertFrom("#EFEFEF")), false);
+            for (int x = 0; x < gameField.GetLength(0); x++)
+                for (int y = 0; y < gameField.GetLength(1); y++)
+                    gameField[x, y] = new Square((SolidColorBrush) (new BrushConverter().ConvertFrom("#EFEFEF")), false);
 
-            SquareSize = Math.Floor(GameCanvas.Height / _gameField.GetLength(1));
+            squareSize = Math.Floor(GameCanvas.Height / gameField.GetLength(1));
 
-            GameCanvas.Width = 10 * SquareSize;
-            GameCanvas.Height = 20 * SquareSize;
+            GameCanvas.Width = 10 * squareSize;
+            GameCanvas.Height = 20 * squareSize;
 
-            LineSize = 1;
+            lineSize = 1;
 
             Line line;
-            for (int column = 0; column < _gameField.GetLength(0) + 1; column++)
+            for (int column = 0; column < gameField.GetLength(0) + 1; column++)
             {
                 line = new Line
                 {
                     Stroke = (SolidColorBrush) (new BrushConverter().ConvertFrom("#000000")),
-                    X1 = column * SquareSize,
-                    X2 = column * SquareSize,
+                    X1 = column * squareSize,
+                    X2 = column * squareSize,
                     Y1 = 0,                  // From start
                     Y2 = GameCanvas.Height, //  To end
-                    StrokeThickness = LineSize
+                    StrokeThickness = lineSize
                 };
                 GameCanvas.Children.Add(line);
             }
             
-            for (int row = 0; row < _gameField.GetLength(1) + 1; row++)
+            for (int row = 0; row < gameField.GetLength(1) + 1; row++)
             {
                 line = new Line
                 {
                     Stroke = (SolidColorBrush)(new BrushConverter().ConvertFrom("#000000")), 
                     X1 = 0,                 // From start
                     X2 = GameCanvas.Width, //  To end
-                    Y1 = row * SquareSize,
-                    Y2 = row * SquareSize,
-                    StrokeThickness = LineSize
+                    Y1 = row * squareSize,
+                    Y2 = row * squareSize,
+                    StrokeThickness = lineSize
                 };
                 GameCanvas.Children.Add(line);
             }
@@ -151,17 +122,14 @@ namespace Tetris
                 RemoveRows();
 
                 // if figure can't be place, GAME OVER
-                foreach (Point cord in _figureArray[NextFigureIndex].Cordinates)
+                if (FigureArray[nextFigureIndex].Cordinates.Any(cord => gameField[(int)cord.X, (int)cord.Y].Filled))
                 {
-                    if (_gameField[(int)cord.X, (int)cord.Y].Filled)
-                    {
-                        FinishGame();
-                        return;
-                    }
+                    FinishGame();
+                    return;
                 }
 
                 makeNewFigure = false;
-                CurrentFigure = _figureArray[NextFigureIndex].Clone();
+                CurrentFigure = FigureArray[nextFigureIndex].Clone();
 
                 // Place cords on field
                 foreach (Point cord in CurrentFigure.Cordinates)
@@ -175,19 +143,19 @@ namespace Tetris
                 bool move = true;
                 for (int i = 0; i < CurrentFigure.Cordinates.Length; i++)
                 {
-                    if (CurrentFigure.Cordinates[i].Y + 1 == _gameField.GetLength(1))
+                    if (CurrentFigure.Cordinates[i].Y + 1 == gameField.GetLength(1))
                         move = false;
-                    else if (_gameField[(int)CurrentFigure.Cordinates[i].X, (int)CurrentFigure.Cordinates[i].Y + 1].Filled)
+                    else if (gameField[(int)CurrentFigure.Cordinates[i].X, (int)CurrentFigure.Cordinates[i].Y + 1].Filled)
                         move = IsPartOfFigure('Y', 1, i);
 
                     if(!move)
                     {
                         makeNewFigure = true;
 
-                        if (NextFigureIndex + 1 == _figureArray.Length)
-                            NextFigureIndex = 0;
+                        if (nextFigureIndex + 1 == FigureArray.Length)
+                            nextFigureIndex = 0;
                         else
-                            NextFigureIndex++;
+                            nextFigureIndex++;
 
                         break;
                     }
@@ -200,15 +168,14 @@ namespace Tetris
         }
         private void ChangeSquareStatus(int x, int y, SolidColorBrush color)
         {
-            _gameField[x, y].Filled = color != GameCanvas.Background;
-
-            _gameField[x, y].Color = color;
+            gameField[x, y].Filled = (color != GameCanvas.Background);
+            gameField[x, y].Color = color;
         }
         private void CheckFirstRow()
         {
-            for(int x = 0; x < _gameField.GetLength(0); x++)
+            for(int x = 0; x < gameField.GetLength(0); x++)
             {
-                if (_gameField[x, 0].Filled)
+                if (gameField[x, 0].Filled)
                 {
                     bool isCurrentSquare = false;
                     foreach (Point cord in CurrentFigure.Cordinates)
@@ -224,32 +191,29 @@ namespace Tetris
         }
         private void RemoveRows()
         {
-            for (int checkRow = 0; checkRow < _gameField.GetLength(1); checkRow++)
+            for (int checkRow = 0; checkRow < gameField.GetLength(1); checkRow++)
             {
                 int filledSquare = 0;
-                for (int checkCol = 0; checkCol < _gameField.GetLength(0); checkCol++)
+                for (int checkCol = 0; checkCol < gameField.GetLength(0); checkCol++)
                 {
-                    if (_gameField[checkCol, checkRow].Filled)
+                    if (gameField[checkCol, checkRow].Filled)
                         filledSquare += 1;
                 }
 
-                if (filledSquare == 10)
+                if (filledSquare < 10) continue;
+                
+                for (int x = 0; x < gameField.GetLength(0); x++)
+                    ChangeSquareStatus(x, checkRow, (SolidColorBrush)GameCanvas.Background);
+
+
+                for(int y = checkRow; y >= 0; y--)
                 {
-                    for (int x = 0; x < _gameField.GetLength(0); x++)
-                        ChangeSquareStatus(x, checkRow, (SolidColorBrush)GameCanvas.Background);
-
-
-                    for(int y = checkRow; y >= 0; y--)
+                    for (int x = 0; x < gameField.GetLength(0); x++)
                     {
-                        for (int x = 0; x < _gameField.GetLength(0); x++)
+                        if (gameField[x, y].Filled)
                         {
-                            if (_gameField[x, y].Filled)
-                            {
-                                ChangeSquareStatus(x, y + 1, _gameField[x, y].Color);
-
-                                ChangeSquareStatus(x, y, (SolidColorBrush)GameCanvas.Background);
-
-                            }
+                            ChangeSquareStatus(x, y + 1, gameField[x, y].Color);
+                            ChangeSquareStatus(x, y, (SolidColorBrush)GameCanvas.Background);
                         }
                     }
                 }
@@ -259,27 +223,27 @@ namespace Tetris
         {
             Rectangle rect;
             _allRect.Clear();
-            for (int x = 0; x < _gameField.GetLength(0); x++)
+            for (int x = 0; x < gameField.GetLength(0); x++)
             {
-                for (int y = 0; y < _gameField.GetLength(1); y++)
+                for (int y = 0; y < gameField.GetLength(1); y++)
                 {
                     rect = new Rectangle
                     {
-                        StrokeThickness = SquareSize - 2,
-                        Stroke = _gameField[x, y].Color // - 2 px from both sides
+                        StrokeThickness = squareSize - 2,
+                        Stroke = gameField[x, y].Color // - 2 px from both sides
                     };
-                    Canvas.SetLeft(rect, x * SquareSize + LineSize);
-                    Canvas.SetTop (rect, y * SquareSize + LineSize);
+                    Canvas.SetLeft(rect, x * squareSize + lineSize);
+                    Canvas.SetTop (rect, y * squareSize + lineSize);
                     _allRect.Add(rect);
                     GameCanvas.Children.Add(rect);
                 }
             }
         }
-        private bool IsPartOfFigure(char cordinate, int direction, int currentIndex)
+        private bool IsPartOfFigure(char coordinate, int direction, int currentIndex)
         {
             for (int figureCheckIndex = 0; figureCheckIndex < CurrentFigure.Cordinates.Length; figureCheckIndex++)
             {
-                if(cordinate == 'Y')
+                if(coordinate == 'Y')
                 {
                     if (CurrentFigure.Cordinates[figureCheckIndex].Y == CurrentFigure.Cordinates[currentIndex].Y + direction &&
                         CurrentFigure.Cordinates[figureCheckIndex].X == CurrentFigure.Cordinates[currentIndex].X &&
@@ -300,7 +264,7 @@ namespace Tetris
             }
             return false;
         }
-        private void MoveFigure(char cordinate, int direction)
+        private void MoveFigure(char coordinate, int direction)
         {
             foreach (Point cord in CurrentFigure.Cordinates)
             {
@@ -309,7 +273,7 @@ namespace Tetris
 
             for (int i = 0; i < CurrentFigure.Cordinates.Length; i++)
             {
-                if(cordinate == 'Y')
+                if(coordinate == 'Y')
                     CurrentFigure.Cordinates[i].Y += direction;
                 else
                     CurrentFigure.Cordinates[i].X += direction;
@@ -321,26 +285,24 @@ namespace Tetris
         private void RotateFigure()
         {
             Point pCord = CurrentFigure.Cordinates[0];
-            List<Point> _newFigure = new List<Point>();
+            List<Point> newFigure = new List<Point>();
             foreach (Point cord in CurrentFigure.Cordinates)
             {
                 Point newPoint = new Point(pCord.X + pCord.Y - cord.Y, cord.X + (pCord.Y - pCord.X));
 
-                if (newPoint.X > 0 && newPoint.X < _gameField.GetLength(0) && 
-                    newPoint.Y > 0 && newPoint.Y < _gameField.GetLength(1) && !_gameField[(int)newPoint.X, (int)newPoint.Y].Filled)
-                    _newFigure.Add(newPoint);
+                if (newPoint.X > 0 && newPoint.X < gameField.GetLength(0) && 
+                    newPoint.Y > 0 && newPoint.Y < gameField.GetLength(1) && !gameField[(int)newPoint.X, (int)newPoint.Y].Filled)
+                    newFigure.Add(newPoint);
             }
-            if(_newFigure.Count == 3)
+            if(newFigure.Count == 3)
             {
                 for (int index = 1; index < CurrentFigure.Cordinates.Length; index++)
                 {
-                    ChangeSquareStatus((int) CurrentFigure.Cordinates[index].X, (int) CurrentFigure.Cordinates[index].Y,
-                        (SolidColorBrush) GameCanvas.Background);
+                    ChangeSquareStatus((int) CurrentFigure.Cordinates[index].X, (int) CurrentFigure.Cordinates[index].Y, (SolidColorBrush) GameCanvas.Background);
 
-                    CurrentFigure.Cordinates[index] = _newFigure[index - 1];
+                    CurrentFigure.Cordinates[index] = newFigure[index - 1];
 
-                    ChangeSquareStatus((int) CurrentFigure.Cordinates[index].X, (int) CurrentFigure.Cordinates[index].Y,
-                        CurrentFigure.Color);
+                    ChangeSquareStatus((int) CurrentFigure.Cordinates[index].X, (int) CurrentFigure.Cordinates[index].Y, CurrentFigure.Color);
                 }
             }
         }
@@ -363,27 +325,27 @@ namespace Tetris
                 }
 
                 if (direction == 2)
-                    gameTimer.Interval = new TimeSpan(0,0,0,0, 10);
-                else
                 {
-                    bool move = true;
-                    for (int index = 0; index < CurrentFigure.Cordinates.Length; index++)
-                    {
-                        if ((direction == 1 && CurrentFigure.Cordinates[index].X + 1 == _gameField.GetLength(0)) ||
-                            (direction == -1 && CurrentFigure.Cordinates[index].X == 0))
-                            move = false;
-                        else if (_gameField[(int)CurrentFigure.Cordinates[index].X + direction, (int)CurrentFigure.Cordinates[index].Y].Filled)
-                        {
-                            if (!IsPartOfFigure('X', direction, index))
-                                move = false;
-                        }
-
-                        if (!move)
-                            break;
-                    }
-                    if (move)
-                        MoveFigure('X', direction);
+                    gameTimer.Interval = new TimeSpan(0,0,0,0, 10);
+                    return;
                 }
+                bool move = true;
+                for (int index = 0; index < CurrentFigure.Cordinates.Length; index++)
+                {
+                    if ((direction == 1 && CurrentFigure.Cordinates[index].X + 1 == gameField.GetLength(0)) ||
+                        (direction == -1 && CurrentFigure.Cordinates[index].X == 0))
+                        move = false;
+                    else if (gameField[(int)CurrentFigure.Cordinates[index].X + direction, (int)CurrentFigure.Cordinates[index].Y].Filled)
+                    {
+                        if (!IsPartOfFigure('X', direction, index))
+                            move = false;
+                    }
+
+                    if (!move)
+                        break;
+                }
+                if (move)
+                    MoveFigure('X', direction);
             }
         }
         private void Window_KeyUp(object sender, KeyEventArgs e)
@@ -397,12 +359,12 @@ namespace Tetris
             gameTimer.Stop();
             gameTimer.Interval = new TimeSpan(0, 0, 0, 0, 100);
 
-            for (int x = 0; x < _gameField.GetLength(0); x++)
+            for (int x = 0; x < gameField.GetLength(0); x++)
             {
-                for (int y = 0; y < _gameField.GetLength(1); y++)
+                for (int y = 0; y < gameField.GetLength(1); y++)
                 {
-                    _gameField[x, y].Filled = false;
-                    _gameField[x, y].Color = (SolidColorBrush) GameCanvas.Background;
+                    gameField[x, y].Filled = false;
+                    gameField[x, y].Color = (SolidColorBrush) GameCanvas.Background;
                 }
             }
 
---
+++ Tetris/Square.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Media;
+
+namespace Tetris
+{
+    public class Square
+    {
+        public SolidColorBrush Color { get; set; }
+        public bool Filled { get; set; }
+        public Square(SolidColorBrush color, bool filled)
+        {
+            this.Color = color;
+            this.Filled = filled;
+        }
+    }
+}
