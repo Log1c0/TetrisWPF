f12c8752fc9a05d8d3d535bb7224a4020ea8b9aa
Log1c0 <log1c05678@gmail.com>
2023-07-17 19:47:14 +0700

    Fix figure empty class

--- Tetris/Figure.cs
+++ Tetris/Figure.cs
@@ -1,7 +1,27 @@
-﻿namespace Tetris
+﻿using System.Windows.Media;
+using System.Windows;
+
+namespace Tetris
 {
     public class Figure
     {
-        
+        public Point[] Cordinates { get; set; }
+        public SolidColorBrush Color { get; set; }
+        public Figure(Point[] Cordinates, SolidColorBrush color)
+        {
+            this.Cordinates = Cordinates;
+            this.Color = color;
+        }
+
+        public Figure Clone()
+        {
+            Figure clone = (Figure)this.MemberwiseClone();
+            clone.Cordinates = new Point[this.Cordinates.Length];
+            for (int i = 0; i < this.Cordinates.Length; i++)
+            {
+                clone.Cordinates[i] = new Point(this.Cordinates[i].X, this.Cordinates[i].Y);
+            }
+            return clone;
+        }
     }
 }
